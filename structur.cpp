9d04f956a0fe01c1617e13e3b2639e32901a26a3
xxx <xxx@gmail.com>
2017-07-30 14:32:58 +0800

    3.0

---
+++ structur.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+struct k{
+	char name[20];
+	float volume;
+	double price;
+};
+
+int main(){
+	using namespace std;
+	k guest={
+		"Glorious Gloria",
+		1.88,
+		29.99
+	};
+
+	k pal={
+		"Audacious Arthur",
+		3.12,
+		32.99
+	};
+
+	cout << "Expand your guest list with " << guest.name << " and " << pal.name << "!" << endl;
+
+	cout << "You can have both fir $" << guest.price+pal.price << endl;
+
+	return 0;
+}
