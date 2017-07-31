04bb66cb8ef0b3175d016921fa90b4532fd80acb
Mr.Tang <2586557491@qq.com>
2017-07-31 18:03:08 +0800

    Update angry.cpp

--- angry.cpp
+++ angry.cpp
@@ -4,19 +4,19 @@ int main(){
 	using namespace std;
 	int i[7],j[7];
 	int n = 0;
-	int max_hungry=0;
+	int max_ungry=0;
 
 	do
 	{
 		cout << "please type learning time" << endl;
 		cin >> i[n] >> j[n];
 		
-		if ( i[n] + j[n] >= max_hungry)
-			max_hungry = i[n]+j[n];
+		if ( i[n] + j[n] >= max_ungry)
+			max_ungry = i[n]+j[n];
 		n++;
 	}while (n<=6);
 	
-	if (max_hungry < 8)
+	if (max_ungry < 8)
 	{
 		cout << "0" << endl;
 		return 1;
@@ -24,7 +24,7 @@ int main(){
 
 	for(int k=0; k<=6; k++)
 	{
-		if (i[k] + j[k] == max_hungry)
+		if (i[k] + j[k] == max_ungry)
 			cout << "The day that most hungry is" << k+1 << endl;
 	}
 
